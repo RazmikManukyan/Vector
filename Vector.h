134a585ec196b0708406882bc0800569f578113e
RazmikManukyan <107432984+RazmikManukyan@users.noreply.github.com>
2023-02-07 19:46:58 +0400

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,4 +1,5 @@
 #ifndef Vector_h
+#include <cstddef>
 #define Vector_h
 
 class Vector {
@@ -10,8 +11,9 @@ public:
 	Vector();
 	Vector(const Vector&);
 	Vector& operator=(const Vector&);
-	Vector(Vector&&) noexcept;
-	Vector& operator=(Vector&&) noexcept;
+	Vector(Vector&&);
+	Vector& operator=(Vector&&);
+	Vector(std::initializer_list<int>);
 	~Vector();
 
 	void push_back(int);
@@ -22,19 +24,16 @@ public:
 	void swap(Vector&);
 	void swap(Vector&, Vector&);
 	int& front();
-	constexpr int& front();
-	const int& front() const;
-	constexpr const int& front() const;
 	int& back();
-	constexpr int& back();
-	const int& back() const;
-	constexpr const int& back() const;
 	int* data();
 	int& operator[](int);
 	const int& operator[](int) const;
 	int& at(std::size_t);
 	const int& at(std::size_t) const;
-
+	int& operator->() const;
+	bool operator==(const Vector&);
+	bool operator!=(const Vector&);
+	bool operator>(const Vector&);
 	std::size_t size() const;
 	std::size_t capacity() const;
 	bool empty() const; 
@@ -42,10 +41,10 @@ public:
 	public:
 		class iterator {
 			public:
-				iterator(int* ptr):ptr(ptr){}
+				iterator(int* ptr):m_iterator(ptr){}
 				
 			private:
-				int* ptr;
+				int* m_iterator;
 		};	
 };
 
